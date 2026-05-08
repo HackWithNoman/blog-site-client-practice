6a9e5193054e6fe7e2b4afb91c4b008a1e2dd2c9
BitWizCoder <mhmdnoman01@gmail.com>
2026-05-08 07:02:17 +0600

    feat: added proxy

--- src/app/(common)/page.tsx
+++ src/app/(common)/page.tsx
@@ -1,12 +1,7 @@
 import Blog from "@/components/home/blog";
 import Hero from "@/components/hero/hero";
-import { userService } from "@/services/user.service";
 
 export default async function Home() {
-  const { data } = await userService.getSession();
-
-  console.log(data);
-
   return (
     <>
       <Hero />
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,27 +1,27 @@
 import { DashboardSidebar } from "@/components/sidebar/app-sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
-const userRole = "admin"
+const userRole = "admin";
 
-const DashboardLayout = ({ admin, user }: {
-    admin: React.ReactNode,
-    user: React.ReactNode
+const DashboardLayout = ({
+  admin,
+  user,
+}: {
+  admin: React.ReactNode;
+  user: React.ReactNode;
 }) => {
-    return (
-
-        <>
-            <SidebarProvider>
-                <div className="relative flex h-dvh w-full">
-                    <DashboardSidebar />
-                    <SidebarInset className="flex flex-col" >
-
-                        {userRole === "admin" ? admin : user}
-                    </SidebarInset>
-                </div>
-            </SidebarProvider>
-        </>
-
-    )
+  return (
+    <>
+      <SidebarProvider>
+        <div className="relative flex h-dvh w-full">
+          <DashboardSidebar />
+          <SidebarInset className="flex flex-col">
+            {userRole === "admin" ? admin : user}
+          </SidebarInset>
+        </div>
+      </SidebarProvider>
+    </>
+  );
 };
 
 export default DashboardLayout;
